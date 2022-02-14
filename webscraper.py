cf843658bda2ed6cf9d6a43500ce6ab6dd3de359
Eli Wald <eliwald@stanford.edu>
2022-02-13 22:18:19 -0800

    added location parameter in call to Bing Module

--- webscraper.py
+++ webscraper.py
@@ -15,11 +15,11 @@ def get_google_results(keyword, location, max_results=10):
     for result in g_results:
         print(result)
 
-def get_bing_results(keyword, mkt='en-US'):
-    make_bing_query(keyword)
+def get_bing_results(keyword, mkt='en-US', location='New York, NY'):
+    make_bing_query(keyword, mkt, location)
 
 
 keyword = 'pizza'
 get_ddg_results(keyword=keyword)
 get_google_results(keyword=keyword, location="Boston,Massachusetts,United States")
-get_bing_results(keyword=keyword)
+get_bing_results(keyword=keyword, mkt='en-US', location='Boston, MA')
